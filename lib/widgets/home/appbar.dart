011d2c199c54624d79370e9ebd15b553ba7206de
venkylm10 <venkylm10@gmail.com>
2023-10-31 02:30:03 +0530

    basic appbar done

--- lib/widgets/home/appbar.dart
+++ lib/widgets/home/appbar.dart
@@ -33,36 +33,41 @@ class _CustomAppBarState extends State<CustomAppBar> {
       duration: const Duration(milliseconds: 200),
       curve: Curves.decelerate,
       height: widget.preferredSize.height + MediaQuery.of(context).padding.top,
-      decoration: BoxDecoration(
-          color: Colors.blue,
+      decoration: const BoxDecoration(
+          color: Color(0xFF4A43EC),
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
       child: Stack(
         children: [
           AppBar(
-            backgroundColor: Colors.transparent,
+            backgroundColor: const Color(0xFF4A43EC),
             elevation: 0,
-            leading: Icon(
+            shape: const RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30))),
+            leading: const Icon(
               Icons.pin_drop_rounded,
               size: 30,
+              color: Colors.white,
             ),
             titleSpacing: -8,
-            title: Column(
+            title: const Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "San Jose, USA",
-                  style: TextStyle(fontSize: 18),
+                  style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
                 Text(
                   "94088",
-                  style: TextStyle(fontSize: 14),
+                  style: TextStyle(fontSize: 14, color: Colors.white),
                 )
               ],
             ),
             actions: [
               if (!widget.isSearching)
                 IconButton(
-                  icon: Icon(Icons.search),
+                  color: Colors.white,
+                  icon: const Icon(Icons.search),
                   onPressed: () {
                     if (!widget.isSearching) {
                       // Only call onSearch when not in searching state
@@ -72,7 +77,8 @@ class _CustomAppBarState extends State<CustomAppBar> {
                   },
                 ),
               IconButton(
-                icon: Icon(Icons.notifications),
+                color: Colors.white,
+                icon: const Icon(Icons.notifications),
                 onPressed: () {
                   // Handle notification button press
                 },
@@ -87,32 +93,32 @@ class _CustomAppBarState extends State<CustomAppBar> {
               right: 0,
               child: Container(
                 height: 50,
-                padding: EdgeInsets.symmetric(horizontal: 10),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
                 color: Colors.transparent,
                 child: TextField(
                   controller: widget.searchController,
                   focusNode: searchFocusNode,
                   decoration: InputDecoration(
                     hintText: 'Search...',
-                    hintStyle: TextStyle(color: Colors.white),
-                    prefixIcon: Icon(Icons.search),
+                    hintStyle: const TextStyle(color: Colors.white),
+                    prefixIcon: const Icon(Icons.search, color: Colors.white),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25),
-                      borderSide: BorderSide(color: Colors.white),
+                      borderSide: const BorderSide(color: Colors.white),
                     ),
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25),
-                      borderSide: BorderSide(color: Colors.white),
+                      borderSide: const BorderSide(color: Colors.white),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25),
-                      borderSide: BorderSide(color: Colors.white),
+                      borderSide: const BorderSide(color: Colors.white),
                     ),
                   ),
                   onChanged: widget.onSearch,
                 ),
               ),
-            )
+            ),
         ],
       ),
     );
