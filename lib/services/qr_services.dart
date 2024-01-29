20a55ebe715184e961d87f0f3cc238fcea21ac49
venylm10 <venkylm10@gmail.com>
2024-01-29 19:46:49 +0530

    Version 0.0.2

--- lib/services/qr_services.dart
+++ lib/services/qr_services.dart
@@ -5,6 +5,8 @@ import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:zipbuzz/models/events/event_model.dart';
+import 'package:zipbuzz/utils/constants/colors.dart';
+import 'package:zipbuzz/utils/constants/globals.dart';
 
 class QrServices {
   static void shareEventQrCode(String link, EventModel event) async {
@@ -35,5 +37,51 @@ class QrServices {
     Share.shareXFiles([XFile(filePath)],
         text:
             "Scan the QR to find more details on ZipBuzz App\nEvent: ${event.title}\nAbout: ${event.about}\nHost: ${event.hostName}\nDate: ${event.date.substring(0, 10)}\nStart Time: ${event.startTime}\nEnd Time: ${event.endTime}\nLocation: ${event.location}");
+  }
+
+  static void showQRImage(String link, EventModel event) async {
+    final image = await QrPainter(
+      data: link,
+      version: QrVersions.auto,
+      gapless: false,
+      embeddedImageStyle: const QrEmbeddedImageStyle(
+        size: Size(120, 120),
+        color: Colors.white,
+      ),
+      eyeStyle: const QrEyeStyle(
+        color: Colors.white,
+        eyeShape: QrEyeShape.square,
+      ),
+      dataModuleStyle: const QrDataModuleStyle(
+        color: Colors.white,
+        dataModuleShape: QrDataModuleShape.circle,
+      ),
+    ).toImage(300);
+
+    final directory = await getExternalStorageDirectory();
+    final filePath = '${directory!.path}/qr_code.png';
+
+    File(filePath).writeAsBytesSync(
+        (await image.toByteData(format: ImageByteFormat.png))!.buffer.asUint8List());
+
+    showDialog(
+        context: navigatorKey.currentContext!,
+        barrierDismissible: true,
+        builder: (context) {
+          final width = MediaQuery.of(context).size.width;
+          return AlertDialog(
+            backgroundColor: AppColors.primaryColor.withOpacity(0.75),
+            surfaceTintColor: Colors.transparent,
+            content: Container(
+              height: width * 0.75,
+              width: width * 0.75,
+              constraints: const BoxConstraints(
+                maxHeight: 300,
+                maxWidth: 300,
+              ),
+              child: Image.file(File(filePath)),
+            ),
+          );
+        });
   }
 }
--- lib/widgets/event_details_page/event_qrcode.dart
+++ lib/widgets/event_details_page/event_qrcode.dart
@@ -14,49 +14,49 @@ class EventQRCode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context,ref,child) {
-        return GestureDetector(
-          onTap: () async {
-            final uri = await ref.read(deepLinkServicesProvider).generateEventDynamicLink(event.id.toString());
-            QrServices.shareEventQrCode(uri.toString(), event);
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 6,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.03),
-                  blurRadius: 1,
-                  spreadRadius: 2,
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SvgPicture.asset(
-                  Assets.icons.qr,
-                  height: 20,
-                ),
-                const SizedBox(width: 5),
-                Text(
-                  "Invite",
-                  style: AppStyles.h5.copyWith(
-                    color: AppColors.primaryColor,
-                  ),
-                ),
-              ],
-            ),
+    return Consumer(builder: (context, ref, child) {
+      return GestureDetector(
+        onTap: () async {
+          final uri = await ref
+              .read(deepLinkServicesProvider)
+              .generateEventDynamicLink(event.id.toString());
+          QrServices.showQRImage(uri.toString(), event);
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 6,
           ),
-        );
-      }
-    );
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.03),
+                blurRadius: 1,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(
+                Assets.icons.qr,
+                height: 20,
+              ),
+              const SizedBox(width: 5),
+              Text(
+                "Invite",
+                style: AppStyles.h5.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
