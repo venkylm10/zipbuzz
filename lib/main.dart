5b4f9b8c093d1f3e0cdd28d1c1d903fd74ed2999
venkylm10 <venkylm10@gmail.com>
2024-05-29 02:08:54 +0530

    android flavors

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_uxcam/flutter_uxcam.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:zipbuzz/services/dio_services.dart';
-import 'package:zipbuzz/services/notification_services.dart';
-import 'package:zipbuzz/utils/constants/colors.dart';
-import 'package:zipbuzz/firebase_options.dart';
 import 'package:zipbuzz/routes.dart';
+import 'package:zipbuzz/utils/constants/colors.dart';
 import 'package:zipbuzz/utils/constants/globals.dart';
 import 'package:zipbuzz/widgets/auth_gate.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-  await GetStorage.init();
-  if (!kIsWeb) {
-    await NotificationServices().initNotifications();
-  }
-  await DioServices.getToken();
-  runApp(const ProviderScope(child: MyApp()));
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
---
+++ lib/main/main_prod.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:zipbuzz/env.dart';
+import 'package:zipbuzz/main.dart';
+import 'package:zipbuzz/services/dio_services.dart';
+import 'package:zipbuzz/services/notification_services.dart';
+import 'package:zipbuzz/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  AppEnvironment.setupEnv(Environment.prod);
+  await GetStorage.init();
+  if (!kIsWeb) {
+    await NotificationServices().initNotifications();
+  }
+  await DioServices.getToken();
+  runApp(const ProviderScope(child: MyApp()));
+}
