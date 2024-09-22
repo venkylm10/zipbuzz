7902a5f2935e68a3dd1469abc0a8846dd2be0791
venylm10 <venkylm10@gmail.com>
2024-09-22 06:01:56 +0530

    fixed google sign in

--- lib/services/firebase_providers.dart
+++ lib/services/firebase_providers.dart
@@ -3,10 +3,10 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 final authProvider = Provider((ref) => FirebaseAuth.instance);
 final databaseProvider = Provider((ref) => FirebaseDatabase.instance);
-final googleSignInProvider = Provider((ref) => GoogleSignIn());
-final appleSignInProvider = Provider((ref) => SignInWithApple());
+final googleSignInProvider = Provider(
+  (ref) => GoogleSignIn(scopes: ['profile', 'email']),
+);
 final storageProvider = Provider((ref) => FirebaseStorage.instance);
