3f03bddf2cf6a86f155dea9b53eaf0b2cf2c3497
Dhiransh <dhiransh@Dhiranshs-MacBook-Pro.local>
2024-10-28 21:49:15 +0530

    Stable Version

--- lib/env.dart
+++ lib/env.dart
@@ -25,7 +25,7 @@ abstract class AppEnvironment {
     _env = env;
 
     /// Update this before every release
-    _appVersion = "1.0.75";
+    _appVersion = "1.0.77";
 
     switch (env) {
       case Environment.dev:
